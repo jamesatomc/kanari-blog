0403785e96d6418f25aff72c20ef2af26633c831
jamesatomc <jiraphonwansai2002@gmail.com>
2024-09-15 19:25:31 +0700

    Refactor Astro config and package.json, and update styles for Kanari SDK Design page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
